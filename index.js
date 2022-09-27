9a56127037021047b2c18441ac2569756888d0c6
prannay <prannaykedia1@gmail.com>
2022-09-27 20:10:50 +0530

    new  template

---
+++ index.js
@@ -0,0 +1,9 @@
+const mobileMenu = document.getElementById("mobile-menu");
+
+function closeMenu() {
+  mobileMenu.classList.add("hidden");
+}
+
+function openMenu() {
+  mobileMenu.classList.remove("hidden");
+}
