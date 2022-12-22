8bd5c3d752174f1b2676eb7b13542dfa1f2f48ce
Amit Chakraborti <amit.chakraborti09@gmail.com>
2022-12-22 22:13:36 +0530

    message

--- index.js
+++ index.js
@@ -7,3 +7,20 @@ function closeMenu() {
 function openMenu() {
   mobileMenu.classList.remove("hidden");
 }
+
+var modal = document.getElementById("myModal");
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+  modal.style.display = "none";
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+}
